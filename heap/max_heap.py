f2a32c0e288876d58dd88a81dea8b49b7ab706d8
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-24 15:14:17 -0500

    :construction: max_heap insert and bubble_up implemented
    Insert test passes OK

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,26 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(value)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        p = (index-1)//2
+        if self.storage[p] > self.storage[index]:
+            self.storage[p], self.storage[index] = self.storage[index], self.storage[p]
+            self._bubble_up(index)
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        l = 2*index+1
+        r = 2*index+2
