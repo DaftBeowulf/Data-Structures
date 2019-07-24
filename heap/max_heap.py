1a2d612041c84121c0b208a2a64d4e35b9fd1bf4
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-24 16:13:10 -0500

    :construction: First pass at .delete(), passing all but 1 test

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -4,23 +4,49 @@ class Heap:
 
     def insert(self, value):
         self.storage.append(value)
-        self._bubble_up(value)
+        self._bubble_up(len(self.storage)-1)
 
     def delete(self):
-        pass
+        # first swap root and last element
+        self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+        # delete last element (former root)
+        del self.storage[-1]
+        # start process for sifting new root down
+        self._sift_down(0)
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        p = (index-1)//2
-        if self.storage[p] > self.storage[index]:
-            self.storage[p], self.storage[index] = self.storage[index], self.storage[p]
-            self._bubble_up(index)
+        if index:
+            p = (index-1)//2
+            if self.storage[p] < self.storage[index]:
+                self.storage[p], self.storage[index] = self.storage[index], self.storage[p]
+                self._bubble_up(p)
 
     def _sift_down(self, index):
         l = 2*index+1
         r = 2*index+2
+
+        # only execute if left child index actually exists in storage, else end
+        if l < self.get_size():
+            # find greatest child value
+            max_child = l
+            # set max as right child if index exists and greater than left child
+            if r < self.get_size():
+                if self.storage[r] > self.storage[l]:
+                    max_child = r
+
+                if self.storage[index] < self.storage[max_child]:
+                    self.storage[index], self.storage[max_child] = self.storage[max_child], self.storage[index]
+                    self._sift_down(max_child)
+
+
+h = Heap()
+h.insert(2)
+h.insert(100)
+h.insert(50)
+h.delete()
